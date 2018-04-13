a008eacf5222f5eb9acfa78507401355c881b6ea
daveyJones49 <davidmckinney914@gmail.com>
2018-04-13 16:53:51 -0500

    GBN_S: Added timers and a dummy receiver testing component
    Still WIP, sender still hanging after initial messages.

--- python/gbn_sender.py
+++ python/gbn_sender.py
@@ -1,52 +1,117 @@
 #!/usr/bin/env python3
 import socket
-import Message
+from Message import Message
+import time
+from threading import Timer
 
 class GbnSender:
-    def __init__(self, windowSize, payloadSize, filePath):
+    def __init__(self, windowSize, filePath):
         self.serverAddr = "127.0.0.1"
         self.serverPort = 2345
-        self.socket = socket(AF_INET, SOCK_DGRAM)
-        self.payloadSize = payloadSize
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.filePath = filePath
-
-        self.window = []        
+        self.payLoadSize = 10
+        self.packetTimers = {}
+        self.window = []
         self.windowBase = 0
         self.windowSize = windowSize
-        self.windowSizeBytes = windowSize * payloadSize
+        self.windowSizeBytes = windowSize * self.payLoadSize
 
         self.retransmitted = 0
 
+    def transmit_packet(self, packet):
+        print('Send: %s', packet.toBytes())
+        self.socket.sendto(packet.toBytes(), (self.serverAddr, self.serverPort))
+
+    def packet_timeout(self, packet):
+        print("I have timed oout")
+        #self.transmit_packet(packet)
+
     def run(self):
+        print("Starting up...")
+        time.sleep(1)
         # retrieve x bytes from file and send 
         with open(self.filePath, "rb") as inputFile:
-            pkt = inputFile.read(self.payloadSize)
-            sendSeqNum = 1
-            sendAckNum = 0
+            pkt = inputFile.read(self.payLoadSize)
+            seqNum = 0
+            ackNum = 0
             while pkt:
                 # if the window isn't full yet
-                if seqNum + 1 < self.windowBase + self.windowSize:
+                if seqNum + 1 <= self.windowBase + self.windowSize:
                     seqNum = seqNum + 1
 
                     # package up into message and send to receiver
-                    msg = Message(sendSeqNum, sendAckNum, pkt)
-                    self.socket.sendTo(msg, (serverAddr, serverPort))
+                    msg = Message(seqNum  = seqNum,
+                                  ackNum  = ackNum,
+                                  payload = pkt )
+                    self.transmit_packet(msg)
 
                     # Read more from file
-                    pkt = inputFile.read(self.payloadSize)
+                    pkt = inputFile.read(self.payLoadSize)
                     self.window.append(pkt)
+
+                    # Set new packet timer with ID ack
+                    self.packetTimers[ackNum] = Timer(0.1, self.packet_timeout, ackNum, pkt)
 
                 # look for an ACK to further along the window
                 else:
-                    msgBytes, Addr = self.socket.recvFrom(2048)
-                    msg = Message(msgBytes)
+                    msgBytes, Addr = self.socket.recvfrom(2048)
+                    msg = Message.setMessage(msgBytes)
 
                     # successfully received uncorrupted packet
                     if msg.checksumValue == msg.calcChecksum():
-                        ReceivedSeqNum = msg.ackNum
                         # move window base along, remove packet from front
+                        # and remove timer for packet
+                        del self.packetTimers[msg.ReceivedAckNum]
                         self.windowBase = self.WindowBase + 1
                         del self.window[0]
-                    
-                # probably put timeout here
+
+# Dummy receiver class to test sender 
+class GbnReceiver:
+    def __init__(self, windowSize):
+        self.serverAddr = "127.0.0.1"
+        self.serverPort = 2345
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.payLoadSize = 10
+        self.cumulativeAck = 0
+        self.window = []
+        self.windowBase = 0
+        self.windowSize = windowSize
+        self.windowSizeBytes = windowSize * self.payLoadSize
+
+    def run(self):
+        # receive
+        print("Starting up...")
+        self.socket.bind((self.serverAddr, self.serverPort))
+        time.sleep(1)
+        while True:
+            print(" I am waiting on packets... ")
+            msgBytes, Addr = self.socket.recvfrom(2048)
+            #msg = Message.fromBytes(msgBytes)
+            print('Received: %s', msgBytes)
+
+            msgBytes, clientAddress = self.socket.recvfrom(2048)
+            msg = Message(messageBytes=msgBytes)
+
+            if msg.checksumValue == msg.calcChecksum():
+                if msg.acknowledgmentNumber == (self.cumulativeAck + 1):
+                    ReceivedAckNum = msg.acknowledgmentNumber
+                    ReceivedSeqNum = msg.sequenceNumber
+
+                    # send acknowledgement if the packet 
+                    msg = Message(ReceivedSeqNum, ReceivedAckNum+1, pkt)
+                    print('Received: %s', msg.payload)
+
+                    self.socket.sendto(msg, (self.serverAddr, self.serverPort))
+
+                else:
+                    # discard packet out of line
+                    print('Message discarded, out of order...')
+                    pass
+
+            time.sleep(1)
+
+
+
+
 
--- python/menu.py
+++ python/menu.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 import selectiveRepeatRunner
-import gbn_sender
+from gbn_sender import GbnSender
+from gbn_sender import GbnReceiver
 import os
+
+# For testing
+from multiprocessing import Process, Queue
 
 def prompt_for_file():
    while True:
         file_name = input("Enter a file path>")
         if os.path.exists(file_name):
             return file_name
-        print("File %s not found.", file_name) 
+        print("File %s not found.", file_name)
 
 def prompt_for_window():
     while True:
-        try: 
+        try:
             window_size = int(input("Enter a window size>"))
-    
+
             if window_size in range(1,65555):
                 return window_size
-            else: 
+            else:
                 print("Invalid window size")
                 print("Acceptable window sizes: integers between 1 and 65,535") 
         except ValueError:
@@ -35,16 +39,32 @@ def main():
             decision = input(">")
 
             if decision == "0":
-                filepath = prompt_for_file()
-                print("Go back N currently unsupported")
-                #gbn = gbn_sender(windowSize, payloadSize, filePath)
-                #gbn.run()
+                #filePath = prompt_for_file()
+                #windowSize = prompt_for_window()
+                filePath = "a.txt"
+                windowSize = 3
+
+                # temporary setup 
+                gbn = GbnSender(windowSize, filePath)
+                gbn_r = GbnReceiver(windowSize)
+
+                ps = Process(target=gbn.run, args=())
+                pr = Process(target=gbn_r.run, args=())
+
+                ps.start()
+                pr.start()
+
+                ps.join()
+                pr.join()
+
                 decision = "x"
+
             elif decision == "1":
-                filepath = prompt_for_file()
-                window_size = prompt_for_window()
+                #filepath = prompt_for_file()
+                #windowSize = prompt_for_window()
                 #selectiveRepeatRunner.run()
                 decision = "x"
+
             elif decision != "x":
                 print("Decision not recognized.\n")
 
@@ -52,5 +72,4 @@ def main():
         print()
     pass
 
-    
 main()
